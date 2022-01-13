c457ce015762639eef7d1c7c378b6aef8b02f6f4
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-12 20:40:09 -0700

    simple counter complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { createSignal } from 'solid-js';
 import styles from './App.module.scss';
 
 function App() {
+  const [count, setCount] = createSignal(0);
+
+  function up() {
+    setCount(count() + 1);
+  }
+
+  function down() {
+    setCount(count() - 1);
+  }
+
   return (
     <div>
-      <div>this</div>
-      <div>is</div>
-      <div>cool</div>
-      <div>new</div>
-      <div>new`</div>
-      <div>new</div>
+      <div>${count}</div>
+      <button onClick={up}>up</button>
+      <button onClick={down}>down</button>
     </div>
   );
 }
